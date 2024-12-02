35823bef1c2414a3a71f063aa469252419bfca27
Randú <134299499+joserandu@users.noreply.github.com>
2024-12-02 09:38:40 -0300

    Create logic.py

---
+++ projetoMarketstock/estoque/logic.py
@@ -0,0 +1,22 @@
+Class Produto:
+  def __init__(qtd, validade, media_vendas):
+    self.__qtd = qtd
+    self.__validade = validade
+    self.__media_vendas
+
+  def getQtd():
+    return qtd
+
+  def setQtd(newQtd):
+    self.__qtd = newQtd
+
+  def getValidade():
+    return validade
+
+  def setValidade(newValidade):
+    self.__validade = newValidade
+
+  def getMedia_vendas():
+    return media_vendas
+
+  
