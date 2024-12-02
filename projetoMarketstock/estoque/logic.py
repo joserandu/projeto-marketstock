a19c8792192f2dad64363ba4b8c02393ee9a055e
Randú <134299499+joserandu@users.noreply.github.com>
2024-12-02 10:43:28 -0300

    Update logic.py

--- projetoMarketstock/estoque/logic.py
+++ projetoMarketstock/estoque/logic.py
@@ -1,9 +1,16 @@
+from random import randint
+import time
+
 Class Produto:
-  def __init__(qtd, validade, media_vendas):
+  def __init__(nome, qtd, validade):
+    self.__nome = nome
     self.__qtd = qtd
     self.__validade = validade
-    self.__media_vendas
+    # self.__media_vendas = media_vendas
+    # self.__alerta_escasses = alerta_escasses
+    # self.__tempo_restante = tempo_restante
 
+  # Getters e Setters
   def getQtd():
     return qtd
 
@@ -15,8 +22,61 @@ Class Produto:
 
   def setValidade(newValidade):
     self.__validade = newValidade
-
+"""
   def getMedia_vendas():
     return media_vendas
 
-  
+  def setMedia_vendas():
+    self.__media_vendas = media_vendas
+
+  def getAlerta_escasses():
+    return alerta_escasses
+
+  def setAlerta_escasses(newAlerta_escasses):
+    self.__alerta_escasses = newAlerta_escasses
+
+  def getTempo_restante():
+    return tempo_restante
+
+  def setTempo_restante(newTempo_restante):
+    self.__tempo_restante = newTempo_restante
+"""
+  # Métodos
+
+  @staticmethod
+  def qtd_vendas():
+    qtd = randint(0, 10)
+    return qtd
+
+  @staticmethod
+  def calcular_media_vendas(): 
+    media = randint(0, 10) / 1  # é nesse 10 que a gente controla quanto é vendido por hora
+
+  def calcular_tempo_restante(self, media_vendas):
+    pass
+
+  def alertar_escasses(self, produto, tempo_restante):  // Já instanciado
+    if (tempo_restante < 5):
+      print(f"O produto: {produto} acabará em {tempo_restante}")
+
+produto = Produto("Paçoca", 20, "20/12/2024")
+
+def main(produto):
+ 
+  lista_vendas = []
+
+  while(produto.qtd >= 0):
+
+    i = 1
+    qtd = produto.qtd_vendas()
+    vendas_hora = {"hora: ": i, "Quantidade de vendas: " = qtd}
+    lista_vendas.append(vendas_hora)
+    
+    print(f"Quantidade de vendas na última  {i}: {qtd}")
+
+    i += 1
+    time.sleep(2)
+    # media = produto.calcular_media_vendas()
+    # printf(f"Media de vendas: ")
+
+main(produto)
